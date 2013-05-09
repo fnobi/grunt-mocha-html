9843bce686d00a0f0f374e533d56a715670aaab2
Fujisawa Shin <fujisawa-shin@in.kayac.com>
2013-05-10 02:22:42 +0900

    だいたい完成。細かい検証は未。

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ grunt-mocha-html
 ================
 
 generate html for mocha browser test.
+
+
+## sample Gruntfile
+
+```javascript:Gruntfile.js
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mocha_html: {
+            all: {
+                src   : [ 'js/lib/*.js', 'js/*.js' ],
+                test  : [ 'test/*-test.js' ],
+                assert : 'chai'
+            }
+        },
+        mocha_phantomjs: {
+            all: ['test/*.html']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-mocha-phantomjs');
+    grunt.loadNpmTasks('grunt-mocha-html');
+
+    grunt.registerTask('test', ['mocha_html', 'mocha_phantomjs']);
+    grunt.registerTask('default', ['test']);
+};
+```
--- tasks/mocha_html.js
+++ tasks/mocha_html.js
@@ -1,13 +1,64 @@
 module.exports = function (grunt) {
-    grunt.registerMultiTask(
-        'mocha_html',
-        'generate html for mocha browser test.',
-        function () {
-            var target = this.target;
-            var done = this.async();
+    var path = require('path'),
+        ejs  = require('ejs');
 
-            var koko = new Koko(process.cwd(), grunt.config('koko')[target]);
-            koko.start();
+    var _ = grunt.util._;
+
+    var taskName        = 'mocha_html',
+        taskDescription = 'generate html for mocha browser test.',
+        taskDir         = 'node_modules/grunt-mocha-html';
+
+    grunt.file.defaultEncoding = 'utf8';
+    var template = grunt.file.read(path.join(taskDir, '/template/runner.html.ejs'));
+
+    grunt.registerMultiTask(taskName, taskDescription, function () {
+        var target = this.target,
+            config = grunt.config(taskName)[target],
+
+            autoClean = config.autoClean || false,
+
+            htmlPath = config.html || 'test/' + target + '.html',
+            dirname  = path.dirname(htmlPath),
+
+            assert     = config.assert || 'assert',
+            assertPath = 'node_modules/' + assert + '/' + assert + '.js',
+
+            srcPaths = config.src.length ? _.flatten(_.map(
+                config.src, function (pattern) {
+                    return grunt.file.expand(pattern);
+                }
+            )) : [],
+
+            testPaths = config.test.length ? _.flatten(_.map(
+                config.test, function (pattern) {
+                    return grunt.file.expand(pattern);
+                }
+            )) : [];
+
+        if (!htmlPath) {
+            throw new Error('invalid html path.');
         }
-    );
+        if (!grunt.file.exists(assertPath)) {
+            throw new Error('assert module is not found. [' + assertPath + ']');
+        }
+
+        var html = ejs.render(template, {
+            title      : config.title || target,
+            cssPath    : path.relative(dirname, 'node_modules/mocha/mocha.css'),
+            mochaPath  : path.relative(dirname, 'node_modules/mocha/mocha.js'),
+            assertPath : path.relative(dirname, assertPath),
+            srcPaths   : _.map(srcPaths, function (srcPath) {
+                return path.relative(dirname, srcPath);
+            }),
+            testPaths   : _.map(testPaths, function (testPath) {
+                return path.relative(dirname, testPath);
+            })
+        });
+
+        grunt.file.write(htmlPath, html);
+
+        if (autoClean) {
+            grunt.file.write(htmlPath);
+        }
+    });
 };
